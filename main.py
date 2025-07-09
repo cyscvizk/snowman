6cb7c745a080e0a3220f5ee2239dca9a9c199835
cyscvizk <cysc.vizk@gmail.com>
2025-07-09 09:58:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from game_logic import play_game
+from game_logic import play_game, ask_replay
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+        if not ask_replay():
+            print("\n👋 Thanks for playing Snowman Meltdown!")
+            break
