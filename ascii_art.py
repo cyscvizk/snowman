3fbb1c2a55c6d5e10f6a257eef80a1e6d21d449d
cyscvizk <cysc.vizk@gmail.com>
2025-07-09 09:57:30 +0200

    Update ascii_art.py

--- ascii_art.py
+++ ascii_art.py
@@ -1,31 +1,54 @@
 # ascii_art.py
 
-# Snowman ASCII Art stages
+# Expanded Snowman ASCII Art stages
 STAGES = [
-    # Stage 0: Full snowman
     """
-      ___
-     /___\\
-     (o o)
-     ( : )
-     ( : )
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
     """,
-    # Stage 1: Bottom part starts melting
     """
-      ___
-     /___\\
-     (o o)
-     ( : )
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     (   ) 
     """,
-    # Stage 2: Only the head remains
     """
-      ___
-     /___\\
-     (o o)
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+      
     """,
-    # Stage 3: Snowman completely melted
     """
-      ___
-     /___\\
+      ___  
+     /___\\ 
+     (o o) 
+     (   ) 
+      
+    """,
+    """
+      ___  
+     /___\\ 
+     (o o) 
+     
+      
+    """,
+    """
+      ___  
+     /___\\ 
+     
+     
+      
+    """,
+    """
+     
+     
+     
+     
+      
     """
 ]
